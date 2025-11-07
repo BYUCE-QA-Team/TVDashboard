bd8bb9f4f62acf2d6cdc95beb6985fa82c13cecc
BYUCE-QA-Team <153956080+BYUCE-QA-Team@users.noreply.github.com>
2025-11-07 12:11:20 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// config.js
+window.dashboardConfig = {
+  // Folder containing slide images
+  slidesFolder: "slides",
+
+  // List of image filenames (relative to slidesFolder)
+  slides: [
+    "slide1.png",
+    "slide2.jpg",
+    "slide3.jpg"
+  ],
+
+  // Power BI report embed URL
+  powerBiEmbedUrl: "https://app.powerbi.com/reportEmbed?reportId=b1935f1f-822c-4676-a96d-d0cb92cf764f&autoAuth=true&ctid=c6fc6e9b-51fb-48a8-b779-9ee564b40413",
+
+  // Duration each slide (and dashboard) is shown in milliseconds
+  slideDuration: 5000 // 15 seconds
+};
