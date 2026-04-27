9b80cf7d4e418c55cf70ab52bbee917d1bb2bfbd
Caleb Dew <calebdew@byu.edu>
2026-04-27 13:12:17 -0600

    Re-did slide naming system and fixed all the broken stuff.

--- config.js
+++ config.js
@@ -5,30 +5,30 @@ window.dashboardConfig = {
 
   // List of image filenames (relative to slidesFolder)
   slides: [
-    "slide1.png",
-    "slide2.png",
-    "slide3.png",
-    "slide4.png",
-    "slide5.png",
-    "slide6.png",
-    "slide7.png",
-    "slide8.png",
-    "slide9.png",
-    "slide10.png",
-    "slide11.png",
-    "slide12.png",
-    "slide13.png",
-    "slide14.png",
-    "slide15.png",
-    "slide16.png",
-    "slide17.png"
+    'intro.png',
+    'audrey.png',
+    // 'ewerton.png',
+    'org-chart.png',
+    'bella.png',
+    'brietta.png',
+    'caleb.png',
+    // 'courtney.png',
+    'hiro.png',
+    'matias.png',
+    'phil.png',
+    'ryan.png',
+    'tyler.png',
+    'valentina.png',
+    'lorelei.png',
+    'ella.png',
+    'jarret.png',
   ],
 
   // Power BI report embed URL
   powerBiEmbedUrl: "https://app.powerbi.com/reportEmbed?reportId=b1935f1f-822c-4676-a96d-d0cb92cf764f&autoAuth=true&ctid=c6fc6e9b-51fb-48a8-b779-9ee564b40413",
 
   // Duration each slide (and dashboard) is shown in milliseconds
-  slideDuration: 10000 // 15 seconds
+  slideDuration: 1000 // 15 seconds
 };
 
 
