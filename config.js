4be781d41db88f2580dbd5d740ab2ee0ec10cf9b
Caleb Dew <calebdew@byu.edu>
2026-04-27 13:17:52 -0600

    Fixed slide duration

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ window.dashboardConfig = {
   powerBiEmbedUrl: "https://app.powerbi.com/reportEmbed?reportId=b1935f1f-822c-4676-a96d-d0cb92cf764f&autoAuth=true&ctid=c6fc6e9b-51fb-48a8-b779-9ee564b40413",
 
   // Duration each slide (and dashboard) is shown in milliseconds
-  slideDuration: 1000 // 15 seconds
+  slideDuration: 10000 // 15 seconds
 };
 
 
