cb162588c2137befe30d6572e3c9bd29f35fbf95
Caleb Dew <calebdew@byu.edu>
2026-05-18 09:06:13 -0600

    Updated the dashboard

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ window.dashboardConfig = {
   ],
 
   // Power BI report embed URL
-  powerBiEmbedUrl: "https://app.powerbi.com/reportEmbed?reportId=b1935f1f-822c-4676-a96d-d0cb92cf764f&autoAuth=true&ctid=c6fc6e9b-51fb-48a8-b779-9ee564b40413",
+  powerBiEmbedUrl: "https://app.powerbi.com/reportEmbed?reportId=95a75401-2a2a-4646-a367-d91056e052e7&autoAuth=true&ctid=c6fc6e9b-51fb-48a8-b779-9ee564b40413",
 
   // Duration each slide (and dashboard) is shown in milliseconds
   slideDuration: 10000 // 15 seconds
